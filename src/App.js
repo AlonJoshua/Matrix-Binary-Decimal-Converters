9602ac9e4596d41362cfb0cedb117a08297245b4
Alon Joshua <alonjoshua@gmail.com>
2020-03-10 16:45:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      decimal: 0
+    }
+  }
+
+  bin2Dec = (bin) => {
+  let decimal=0;
+  for (var index=bin.length-1; index >=0; index--) {
+    decimal += parseInt(bin[index])*Math.pow(2, bin.length-1-index);
+  }
+  return decimal;
+}
+
+  render() {
+    return (
+      <fragment>
+      <h1>Hello World</h1>
+      </fragment>
+      );
+  }
 }
 
 export default App;
