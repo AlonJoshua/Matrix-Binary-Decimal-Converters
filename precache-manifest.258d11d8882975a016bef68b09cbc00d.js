f68b6b8a57408d6e6f01db058bea20856d4708ca
Alon Joshua <alonjoshua@gmail.com>
2020-04-10 17:04:22 +0300

    Updates

---
+++ precache-manifest.258d11d8882975a016bef68b09cbc00d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a3f95fb652f00078cdf1b3f424fd7b",
+    "url": "/Matrix-Binary-Decimal-Converters/index.html"
+  },
+  {
+    "revision": "6766fff506a8198940f9",
+    "url": "/Matrix-Binary-Decimal-Converters/static/css/main.5f6738f5.chunk.css"
+  },
+  {
+    "revision": "817d69a07e81b3263712",
+    "url": "/Matrix-Binary-Decimal-Converters/static/js/2.5d4a8d98.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Matrix-Binary-Decimal-Converters/static/js/2.5d4a8d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6766fff506a8198940f9",
+    "url": "/Matrix-Binary-Decimal-Converters/static/js/main.ad99c52b.chunk.js"
+  },
+  {
+    "revision": "0acf3eb486fc3ae2778e",
+    "url": "/Matrix-Binary-Decimal-Converters/static/js/runtime-main.ffec76c0.js"
+  }
+]);
